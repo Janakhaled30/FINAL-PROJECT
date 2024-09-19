f8adc30cbd187ecf9639df2b43eacadb170d0176
Hana Mohamed Solaiman <149798813+Solaiman6047@users.noreply.github.com>
2024-09-19 23:50:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DisplayPage from './pages/DisplayPage';
+import Place from './pages/Place';
+import Home from './pages/Home';
 import SuggestedPlaces from './components/SuggestedPlaces';
 
 function App() {
   return (
-    <div>
-      <SuggestedPlaces />
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route index element = {<SuggestedPlaces />} />
+      <Route path='/home' element = {<Home />} />
+      <Route path='/display' element = {<DisplayPage />} />
+      <Route path='/place' element = {<Place />} />
+    </Routes>
+  </BrowserRouter>
 );
 }
 
