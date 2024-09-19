b63dc47854377959626680ce2ec8a3cb3c91f93f
Hana Mohamed Solaiman <149798813+Solaiman6047@users.noreply.github.com>
2024-09-19 23:46:38 +0300

    Update SuggestedPlaces.jsx

--- src/components/SuggestedPlaces.jsx
+++ src/components/SuggestedPlaces.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from "./Card";
 
+const places=['beach','cafe',];
+const RainPlaces=['cafe','library']
+
 function SuggestedPlaces() {
     const [weather, setWeather] = useState(null);
     const api = "https://api.openweathermap.org/data/2.5/weather?q=alexandria&appid=e3f9787e87e2a1b46edcb0c315830d35&units=metric";
@@ -11,7 +14,7 @@ function SuggestedPlaces() {
             .then(response => response.json())
             .then(data => setWeather(data.weather[0].main))
             .catch(error => console.error('Error fetching weather data:', error));
-    }, []);
+    },);
 
     const giveSuggestion = () => {
         if (weather === null) {
@@ -19,49 +22,41 @@ function SuggestedPlaces() {
         }
         else if (weather === 'Rain') {
             return (
-                <Card
-                placeName="Beautiful Cafe"
-                placeImage="le" 
-            />
+                <div className="container text-center">
+                <div className="row g-2">
+                    {RainPlaces.map((place,image, index) => {
+                        return (
+                            <div className="col-6" key={index}>
+                                <div className="p-3">
+                                    <Card
+                                        placeName={place} 
+                                        placeImage={image}
+                                    />
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
             );
         } else {
             return (
-                <div class="container text-center">
-                    <div class="row g-2">
-                        <div class="col-6">
-                            <div class="p-3">      
-                                <Card
-                                    placeName="Beautiful Beach"
-                                    placeImage="le" 
-                                />
+                <div className="container text-center">
+                <div className="row g-2">
+                    {places.map((place,image, index) => {
+                        return (
+                            <div className="col-6" key={index}>
+                                <div className="p-3">
+                                    <Card
+                                        placeName={place} 
+                                        placeImage={image}
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    <div class="col-6">
-                        <div class="p-3">                
-                            <Card
-                                placeName="Beautiful Beach"
-                                placeImage="le" 
-                            />
-                        </div>
-                        </div>
-                        <div class="col-6">
-                            <div class="p-3">                
-                                <Card
-                                    placeName="Beautiful Beach"
-                                    placeImage="le" 
-                            />
-                            </div>
-                        </div>
-                        <div class="col-6">
-                            <div class="p-3">                
-                                <Card
-                                    placeName="Beautiful Beach"
-                                    placeImage="le" 
-                                />
-                            </div>
-                        </div>
-                    </div>
+                        );
+                    })}
                 </div>
+            </div>
             );
         }
     };
