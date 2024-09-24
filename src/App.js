c700d6f169577365a84ec4fff103b4d3413d6e59
ahmed <ahmedmax2818@gmail.com>
2024-09-24 20:50:36 +0300

    login and sign up components

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import DisplayPage from './pages/DisplayPage';
 import Place from './pages/Place';
 import Home from './pages/Home';
 import SuggestedPlaces from './components/SuggestedPlaces';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route index element = {<SuggestedPlaces />} />
+      <Route index element = {<Signup />} />
       <Route path='/home' element = {<Home />} />
       <Route path='/display' element = {<DisplayPage />} />
       <Route path='/place' element = {<Place />} />
---
+++ src/components/Login.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './style.css'
+
+function Login() {
+    return (
+        <div className='login template d-flex justify-content-center align-items-center vh-100 bg-primary'>
+            <div className='form_container p-5 rounded bg-white'>
+            <form>
+                <h3 className='text-center'>Sign In</h3>
+                <div className='mb-2'>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" placeholder='Enter Email' className='form-control'/>
+                </div>
+                <div className='mb-2'>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" placeholder='Enter Password' className='form-control'/>
+                </div>
+                <div className='mb-2'>
+                    <input type="checkbox" className='custom-control custom-checkbox'id="check"/>
+                    <label htmlFor="check" className='custom-input-label ms-2'>
+                        Remember me
+                    </label>
+                </div>
+                <div className='d-grid'>
+                    <button className='btn btn-primary'>Sign In</button>
+                </div>
+                <p className='text-end mt-2'>
+                    Forgot <a href="">Password</a><Link to="/signup" className='ms-2'>Sign up</Link>
+                </p>
+            </form>
+        </div>
+    </div>
+    )
+}
+
+export default Login;
