ca12dfd5473d8a076fea287992476d38d6626b09
Hana Mohamed Solaiman <149798813+Solaiman6047@users.noreply.github.com>
2024-09-28 18:58:07 +0300

    Update Explore.jsx

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -10,42 +10,35 @@ function Explore() {
     const getFamilyPlaces = async () => {
         try {
             const response = await axios.get(`http://127.0.0.1:5000/get_data`, null, {
-                
+
             });
-        // console.log(kids);
-        console.log(response.data);
+            // console.log(kids);
+            console.log(response.data);
 
-        setPlaces(response.data);
-    } catch (error) {
-        console.error("Error fetching family places", error);
-    }
-};
+            setPlaces(response.data);
+        } catch (error) {
+            console.error("Error fetching family places", error);
+        }
+    };
 
-useEffect(() => {
-    getFamilyPlaces();
-}, []);
+    useEffect(() => {
+        getFamilyPlaces();
+    }, []);
 
     return (
-        <div>
-            <Header />
-            <div className='main'>
-                    <div/>
-                <div className="container text-center">
-                <div className="row g-2">
-                    {places.map((place, index) => (
-                        <div className="col-6" key={index}>
-                            <div className="p-3">
-                                <Card
-                                    placeName={place.name}
-                                    placeImage={place.image_url}
-                                />
-                            </div>
+        <div className="container text-center">
+            <div className="row g-2">
+                {places.map((place, index) => (
+                    <div className="col-6" key={index}>
+                        <div className="p-3">
+                            <Card
+                                placeName={place.name}
+                                placeImage={place.image_url}
+                            />
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
-                </div>
-            <Footer />
         </div>
     );
 }
