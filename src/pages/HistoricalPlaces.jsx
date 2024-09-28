175dd13933868394a6c3adb936381d6dfdde8c07
Hana Mohamed Solaiman <149798813+Solaiman6047@users.noreply.github.com>
2024-09-28 19:14:01 +0300

    Update HistoricalPlaces.jsx

--- src/pages/HistoricalPlaces.jsx
+++ src/pages/HistoricalPlaces.jsx
@@ -11,55 +11,7 @@ function Historical() {
     const getFamilyPlaces = async (historical) => {
         try {
             const response = await axios.post(`http://127.0.0.1:5000/history`, null, {
-                params: {import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Card from "../components/Card";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-
-function Historical() {
-    const [places, setPlaces] = useState([]);
-    const historical = "true"
-
-    const getFamilyPlaces = async (historical) => {
-        try {
-            const response = await axios.post(`http://127.0.0.1:5000/history`, null, {
                 params: {
-                    historical: historical
-                }
-            });
-            console.log(response.data);
-
-            setPlaces(response.data);
-        } catch (error) {
-            console.error("Error fetching historical places", error);
-        }
-    };
-
-    useEffect(() => {
-        getFamilyPlaces(historical);
-    }, [historical]);
-
-    return (
-            <div className="container text-center">
-                <div className="row g-2">
-                    {places.map((place, index) => (
-                        <div className="col-6" key={index}>
-                            <div className="p-3">
-                                <Card
-                                    placeName={place.name}
-                                    placeImage={place.image_url}
-                                />
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-    );
-}
-
-export default Historical;
-
                     historical: historical
                 }
             });
