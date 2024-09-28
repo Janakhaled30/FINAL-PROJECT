3fda72acfce01839205e957576634de3139d4247
Hana Mohamed Solaiman <149798813+Solaiman6047@users.noreply.github.com>
2024-09-28 19:02:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HiddenGems from './pages/HiddenGems';
-import Place from './pages/Place';
-import Home from './pages/Home';
-import Family from './pages/Family';
-import Explore from './pages/Explore';
-import Historical from './pages/HistoricalPlaces';
+import HiddenGems from './pages/HiddenGems.jsx';
+import Place from './pages/Place.jsx';
+import Home from './pages/Home.jsx';
+import Family from './pages/Family.jsx';
+import Explore from './pages/Explore.jsx';
+import Historical from './pages/HistoricalPlaces.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
     <BrowserRouter>
     <div className='full'>
+    <Header />
+      <div className='main'>
     <Routes>
       <Route index element = {<Home />} />
       <Route path='/home' element = {<Home />} />
@@ -21,6 +25,8 @@ function App() {
       <Route path='/pages/historical' element = {<Historical />} />
       <Route path='/place/:placeName' element={<Place />} />
       </Routes>
+      </div>
+      <Footer />
       </div>
   </BrowserRouter>
 );
