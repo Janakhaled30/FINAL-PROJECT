b2479c12621fee87ca873a3b43078fc35f5d6ca6
Hana Mohamed Solaiman <149798813+Solaiman6047@users.noreply.github.com>
2024-09-28 19:14:29 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import SuggestedPlaces from "../components/SuggestedPlaces";
-import Ss from "../components/ss";
 
 function Home(){
     return(
         <div style={{textAlign:'left'}}>
-            <Ss/>
             <SuggestedPlaces/>
         </div>
     )
