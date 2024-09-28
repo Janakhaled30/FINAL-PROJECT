88fe090098a744daba4e3f645504dd659b7399cd
Hana Mohamed Solaiman <149798813+Solaiman6047@users.noreply.github.com>
2024-09-28 22:38:15 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import SuggestedPlaces from "../components/SuggestedPlaces";
 function Home(){
     return(
         <div style={{textAlign:'left'}}>
-            <h1 style={{textAlign:center,color:#183661 , fontSize:40px , margin:24px 0px, fontWeight:800}}>Welcome to Alexplorer</h1>
+            <h1 style={{textAlign:`center`,color:`#183661` , fontSize:`40px` , margin:`24px 0px`, fontWeight:`800`}}>Welcome to Alexplorer</h1>
             <SuggestedPlaces/>
         </div>
     )
