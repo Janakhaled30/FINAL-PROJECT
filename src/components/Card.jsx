4f0aeadb8f9b2da2b73fb481aa6e8da242864929
Hana Mohamed Solaiman <149798813+Solaiman6047@users.noreply.github.com>
2024-09-28 18:57:03 +0300

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,19 +4,16 @@ import './cardStyle.css'
 
 
 function Card(props) {
-    const { placeName, placeImage} = props;
+    const { placeName, placeImage,placeInfo} = props;
 
     return (
-  <div class="card" style={{ 
-    width: 500, 
-    // height: 300, 
-
+  <div className="card" style={{ 
     backgroundImage: `url(${placeImage})` 
 }}>
-    <div class="content">
-      <h2 class="title">{placeName}</h2>
-      <img className='cardimg' src={placeImage} alt={placeName} />
-      {/* <p class="copy">{placeInfo}</p> */}
+    <div className="content">
+      <h2 className="title">{placeName}</h2>
+      <img className='CardImage' src={placeImage} alt={placeName} />
+      <p className="copy">{placeInfo}</p>
       <a href={`/place/${placeName}`} className="btn">Wanna visit</a>
       </div>
   </div>
