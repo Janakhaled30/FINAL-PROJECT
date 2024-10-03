27655a603e79aeca0eba05f8efaa28730abea768
Hana Mohamed Solaiman <149798813+Solaiman6047@users.noreply.github.com>
2024-10-03 21:32:06 +0300

    Update Cafe.jsx

--- src/pages/Cafe.jsx
+++ src/pages/Cafe.jsx
@@ -1,29 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from 'axios'; //axios library to commute with the backend
 import Card from "../components/Card";
 
 
 function Cafe() {
     const [cafes, setCafes] = useState([]);
-  
+
+    // get data from the backend
     useEffect(() => {
       axios.get('http://localhost:5000/cafe')
         .then(response => {
-          // Handle success
             setCafes(response.data);
-            console.log("hiii");
             console.log(response.data);
-        })
+        }) //in error case
         .catch(err => {
           console.error('Error fetching data');
         });
     }, []);
 
-    return (
+    return ( // return cards
             <div className="container text-center">
                 <div className="row g-2">
-                    {cafes.map((place, index) => (
-                        <div className="col-6" key={index}>
+                    {cafes.map((place) => (
+                        <div className="col-6">
                             <div className="p-3">
                                 <Card
                                     placeName={place.name}
