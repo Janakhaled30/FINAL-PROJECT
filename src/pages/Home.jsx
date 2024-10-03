4297527179e644e349fd0e3005c747dc143b42e1
Hana Mohamed Solaiman <149798813+Solaiman6047@users.noreply.github.com>
2024-10-03 21:51:23 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import SuggestedPlaces from "../components/SuggestedPlaces";
+import SuggestedPlaces from "../components/SuggestedPlaces.jsx";
+import Carousel from "../components/Carousel.jsx";
+
 
 function Home(){
     return(
-        <div style={{textAlign:'left'}}>
+        <div>
             <h1 style={{textAlign:`center`,color:`#183661` , fontSize:`40px` , margin:`24px 0px`, fontWeight:`800`}}>Welcome to Alexplorer</h1>
+            <Carousel/>
             <SuggestedPlaces/>
         </div>
     )
